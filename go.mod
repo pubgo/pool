aecb34190e9e81f254b6c65ad3010580017c78b9
kooksee <kooksee@163.com>
2019-07-02 12:59:32 +0800

    update test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pubgo/pool
+
+go 1.12
+
+require github.com/pubgo/errors v0.2.1
--- pool_test.go
+++ pool_test.go
@@ -1,1 +1,15 @@
-package pool
+package pool_test
+
+import (
+	"github.com/pubgo/errors"
+	"github.com/pubgo/pool"
+	"testing"
+)
+
+func TestBytesBufferGet(t *testing.T) {
+	buf := pool.BytesBufferGet()
+	defer pool.BytesBufferPut(buf)
+
+	buf.WriteString("hello")
+	errors.T(buf.String() != "hello", "error")
+}
